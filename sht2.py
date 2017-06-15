6163eb11b34f5c79d8c03deb94e98f8d16dfd536
Petr Šesták <psestak@psestak.dev.office.nic.cz>
2017-06-15 12:46:01 +0200

    muj commit

---
+++ sht2.py
@@ -0,0 +1,434 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 11:07:07 2017
+
+@author: jindra
+"""
+ex = '''Problémy s tiskárnami
+Mimoradne vysoky pocet SMS z dohledoveho cisla
+Problémy s tiskárnami
+Pořízení nového HW
+Zřízení emailových schránek
+Přístup na postgresql databázi
+Potíže se servery
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Odstranění/přidání uživatelů a také úprava v RT
+Odstranění/přidání uživatelů a také úprava v RT
+Potíže se servery
+Opravy HW
+Nastavení DNS
+Problémy s OS Ubuntu
+Nefungují obrázky na blog.nic.cz při přístupu přes HTTPS
+IPv6 BGP na BIRDlab
+Pořízení nového HW
+Přesun honeynetu z LABS racku a adresního prostoru
+Potíže se servery
+Statistika na CSIRT
+Nastavení DNS
+Vytvoření e-mailového aliasu
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vytvoření nebo prodloužení certifikátu na VPN
+Nastavení Daphne
+Práce souvisejíci s Akademií CZ.NIC
+Vytvoření e-mailového aliasu
+Nastavení DNS
+Práce nebo potíže související s certifikáty
+Nový certifikát pro registrátora
+Potíže se servery
+Potíže se servery
+Práce související s Jabberem
+Moodle - nechodí e-maily při zápisu do kurzu a z aktivit
+Práce souvisejíci s Akademií CZ.NIC
+iMac - přeinstalace
+Práce souvisejíci s Akademií CZ.NIC
+Práce souvisejíci s Akademií CZ.NIC
+Práce souvisejíci s Akademií CZ.NIC
+Práce související s Captcha help
+Problémy s tiskárnami
+Problémy s tiskárnami
+Problémy s e-maily
+Odstranění/přidání uživatelů a také úprava v RT
+Nastavení DNS
+Práce souvisejíci s Akademií CZ.NIC
+Potíže se servery
+Příslušenství k notebooku (baterie, tašky)
+Potíže se servery
+Příslušenství k notebooku (baterie, tašky)
+Nový certifikát pro registrátora
+Opravy HW
+Příprava validačního notebooku
+Nastavení DNS
+Chyby v evidence.nic.cz
+Práce souvisejíci s mailing listem
+Nastavení DNS
+Problémy s OS Ubuntu
+Pristupy Adriana Petrovova
+Práce související s Captcha help
+Odstranění/přidání uživatelů a také úprava v RT
+Práce související s Captcha help
+Nastavení DNS
+Nový certifikát pro registrátora
+Nastavení DNS
+Problémy s OS Ubuntu
+Práce souvisejíci s Akademií CZ.NIC
+Odstranění/přidání uživatelů a také úprava v RT
+Potíže se servery
+Nastavení DNS
+Pořízení nového HW
+Práce nebo potíže související s OTRS
+Práce nebo potíže související s certifikáty
+Práce souvisejíci s mailing listem
+Práce souvisejíci s Akademií CZ.NIC
+Vytvoření e-mailového aliasu
+Problémy s OS Ubuntu
+Pořízení nového HW
+Problémy s OS Ubuntu
+Opravy HW
+Částečně nefunkční IPv6 v LABsech
+Práce nebo potíže související s certifikáty
+Práce nebo potíže související s certifikáty
+Nastavení DNS
+Vytvoření e-mailového aliasu
+Problémy s tiskárnami
+Presmerovani na webserveru labs.nic.cz
+Vybaveni pracoviště
+Potíže se servery
+Práce nebo potíže související s OTRS
+Problémy s e-maily
+Potíže se servery
+Problémy s e-maily
+Potíže se servery
+Potíže se servery
+Directory listing
+Statistika telefonnich hovoru
+Odstranění/přidání uživatelů a také úprava v RT
+Problematika týkající se Piwiku
+Nastavení DNS
+Práce nebo potíže související s certifikáty
+PTR pro 2001:1488:ffff::60
+Problémy s e-maily
+Nahrazení IPv6 widgetu na test-ipv6.cz
+Plošné zavedení 802.1x
+Práce nebo potíže související s certifikáty
+Potíže se servery
+Potíže se servery
+Práce nebo potíže související s OTRS
+Problémy s OS Ubuntu
+Potíže se servery
+Nastavení DNS
+Přidání nových grafů do stats.nic.cz
+Potíže se servery
+Nastavení Daphne
+Nastavení DNS
+Nastavení DNS
+Vytvoření e-mailového aliasu
+Práce souvisejíci s Akademií CZ.NIC
+Nahrani souboru na server
+Nasazení Knot Resolver 1.2.0 na ODVR včetně DNS over TLS
+Nastavení DNS
+Nastavení DNS
+Statistika na CSIRT
+Vytvoření nebo prodloužení certifikátu na VPN
+Crewatch -chybný výpočet zůstatku dovolené.
+Problémy s tiskárnami
+Vytvoření nebo prodloužení certifikátu na VPN
+Tracmor - Nutnost delat Check-out, check-in jednou osobou
+Nastavení automatické odpovědi – wiki
+Práce souvisejíci s Akademií CZ.NIC
+Nastavení DNS
+Různé kampaně
+Přidání/změna IP adres pro registrátora
+Přidání/změna IP adres pro registrátora
+Potíže se servery
+Potíže se servery
+Matalog test - Internal Server Error
+Přidání/změna IP adres pro registrátora
+Nastavení DNS
+Přidání/změna IP adres pro registrátora
+Implementace offline záloh dat z Labs/Turrisu
+Různé kampaně
+Přidání/změna IP adres pro registrátora
+Nefunguje logování do www.nic.cz/manager, testovací prostředí
+Vložení metadat - SAML
+Přidání/změna IP adres pro registrátora
+Nastavení DNS
+Nahrání prezentací na server
+Potíže související se SOGo
+Chyba 404
+Nový certifikát pro registrátora
+Nastavení Daphne
+Vložení metadat - SAML
+Nahrání nové knihy CyberCrime na web
+Přidání/změna IP adres pro registrátora
+Nastavení DNS
+Nový certifikát pro registrátora
+Zkušební přístup do wiki.nic.cz
+Nahrání ePub a mobi nové knihy CyberCrime
+Různé kampaně
+Administrativni verifikace kontaktu - statistiky
+OPTYS - neodešla data pro tisk dopisu PIN3
+Dočasné otevření přístupu k nastavení zabezpečovačky
+Nový certifikát pro registrátora
+Potíže se servery
+Opravy HW
+Problémy s e-maily
+Nedostupná síť přes VPN
+Odstranění/přidání uživatelů a také úprava v RT
+Ubuntu mirror http://cz.archive.ubuntu.com/ hlásí forbidden
+Odstranění/přidání uživatelů a také úprava v RT
+Odstranění/přidání uživatelů a také úprava v RT
+Problémy s tiskárnami
+Problémy s OS Ubuntu
+Žádost o pluginy do Tracu
+Problémy s tiskárnami
+Nová IP adresa pro registrátora
+Odstranění/přidání uživatelů a také úprava v RT
+Nefunkční přístup do intranet.nic.cz
+Pořízení nového HW
+Práce nebo potíže související s certifikáty
+Pořízení nového HW
+Problémy s tiskárnami
+Opravy HW
+Práce nebo potíže související s rookem
+Vytvoření e-mailového aliasu
+Problémy s tiskárnami
+IT 16 - konektivita
+Práce související s Jabberem
+Problémy s OS Ubuntu
+Aktualizace fotek lektorů
+Příprava validačního notebooku
+Příslušenství k notebooku (baterie, tašky)
+Práce nebo potíže související s OTRS
+Instalace 'SyntaxHighlighter Evolved' na blog.nic.cz
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Příslušenství k notebooku (baterie, tašky)
+Pořízení nového HW
+Problémy s tiskárnami
+Vybaveni pracoviště
+Vytvoření e-mailového aliasu
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Problémy s tiskárnami
+Vytvoření nebo prodloužení certifikátu na VPN
+Příprava validačního notebooku
+Problémy s tiskárnami
+Problémy s tiskárnami
+Vytvoření nebo prodloužení certifikátu na VPN
+Problematika týkající se Piwiku
+Odstranění/přidání uživatelů a také úprava v RT
+Problémy s tiskárnami
+Problémy s tiskárnami
+Problematika týkající se Piwiku
+Problematika týkající se Piwiku
+Problematika týkající se Piwiku
+Práce nebo potíže související s certifikáty
+Příslušenství k notebooku (baterie, tašky)
+Vytvoření e-mailového aliasu
+Nastavení Daphne
+Odstranění/přidání uživatelů a také úprava v RT
+Odstranění/přidání uživatelů a také úprava v RT
+Nový certifikát pro registrátora
+Nefunkční některá písmenka na Microsoft Wireless Sculpt Desktop
+Vytvoření nebo prodloužení certifikátu na VPN
+Vytvoření e-mailového aliasu
+Pořízení nového HW
+Zajištění monitorů pro výrobní linku Omnie
+Vytvoření nebo prodloužení certifikátu na VPN
+Pořízení nového HW
+Odstranění/přidání uživatelů a také úprava v RT
+Vytvoření e-mailového aliasu
+Pořízení nového HW
+Problémy s tiskárnami
+Vytvoření e-mailového aliasu
+Odstranění/přidání uživatelů a také úprava v RT
+Vytvoření e-mailového aliasu
+Potíže se servery
+Potíže se servery
+Pořízení nového HW
+Pořízení nového HW
+Vytvoření nebo prodloužení certifikátu na VPN
+Problémy s tiskárnami
+Nový certifikát pro registrátora
+Potíže se servery
+Nový certifikát pro registrátora
+Problémy s e-maily
+Problémy s e-maily
+Problémy s e-maily
+Vytvoření e-mailového aliasu
+Problémy s tiskárnami
+Vytvoření e-mailového aliasu
+Vytvoření nebo prodloužení certifikátu na VPN
+Potíže se servery
+Vybaveni pracoviště
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vytvoření e-mailového aliasu
+Nový certifikát pro registrátora
+Pořízení nového HW
+Vytvoření e-mailového aliasu
+Práce nebo potíže související s rookem
+Vytvoření e-mailového aliasu
+Vytvoření e-mailového aliasu
+Problémy s OS Ubuntu
+Pořízení nového HW
+Problémy s OS Ubuntu
+Práce nebo potíže související s rookem
+Práce nebo potíže související s rookem
+Vytvoření e-mailového aliasu
+Připojení telefonu do vnitřní sítě (cerifikáty)
+Vytvoření e-mailového aliasu
+Problémy s tiskárnami
+Odstranění/přidání uživatelů a také úprava v RT
+Vytvoření nebo prodloužení certifikátu na VPN
+Pořízení nového HW
+Práce související s Jabberem
+Změna přesměrování na labs.nic.cz
+Připojení telefonu do vnitřní sítě (cerifikáty)
+Vybaveni pracoviště
+Práce nebo potíže související s certifikáty
+Pořízení nového HW
+Potíže se servery
+Podzimní kampaň mojeID 2016 - mailing
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Problémy s tiskárnami
+Administrace Moodle CZ.NIC
+Vytvoření e-mailového aliasu
+Problémy s tiskárnami
+Program pro aktivaci čipu
+Problémy s tiskárnami
+Vytvoření e-mailového aliasu
+Práce nebo potíže související s rookem
+Opravy HW
+IT 16 – konektivita
+Problémy s e-maily
+Nastavení Daphne
+Problémy s tiskárnami
+Pořízení nového HW
+Práce nebo potíže související s rookem
+Pořízení nového HW
+Pořízení nového HW
+Odstranění/přidání uživatelů a také úprava v RT
+Problémy s OS Ubuntu
+Odstranění/přidání uživatelů a také úprava v RT
+Problémy s OS Ubuntu
+Práce nebo potíže související s OTRS
+Problémy s OS Ubuntu
+Práce související s Jabberem
+Vytvoření e-mailového aliasu
+Problémy s e-maily
+Příprava validačního notebooku
+Potíže se servery
+Příprava validačního notebooku
+Potíže související s notebookem
+Příprava validačního notebooku
+Pořízení nového HW
+Problémy s tiskárnami
+Nedostupná síť přes VPN
+Práce souvisejíci s mailing listem
+Potíže související s notebookem
+Pořízení nového HW
+Práce nebo potíže související s OTRS
+Vytvoření e-mailového aliasu
+Příslušenství k notebooku (baterie, tašky)
+Zranitelnost v Roundcube 1.2.2: Command Execution via Email
+Vytvoření nebo prodloužení certifikátu na VPN
+Práce nebo potíže související s rookem
+Potíže související se SOGo
+Problémy s OS Ubuntu
+Potíže související se SOGo
+Potíže související se SOGo
+Zřízení emailových schránek
+Vytvoření nebo prodloužení certifikátu na VPN
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vytvoření e-mailového aliasu
+Vytvoření nebo prodloužení certifikátu na VPN
+Vytvoření e-mailového aliasu
+Vytvoření e-mailového aliasu
+Potíže související se SOGo
+Práce nebo potíže související s rookem
+Vytvoření e-mailového aliasu
+Práce nebo potíže související s certifikáty
+Vytvoření nebo prodloužení certifikátu na VPN
+Chyby CRM
+Chyby CRM
+Práce souvisejíci s mailing listem
+Vytvoření e-mailového aliasu
+Práce nebo potíže související s rookem
+Práce nebo potíže související s certifikáty
+Opravy HW
+Práce souvisejíci s Akademií CZ.NIC
+Problémy s tiskárnami
+Opravy HW
+MojeID newsletter 2017
+Práce souvisejíci s mailing listem
+Nedostupná síť přes VPN
+Chyby CRM
+Chyby CRM
+Vytvoření e-mailového aliasu
+Vytvoření e-mailového aliasu
+Ubuntu mirror http://cz.archive.ubuntu.com/ hlásí forbidden
+Vytvoření e-mailového aliasu
+Potíže související se SOGo
+Práce nebo potíže související s certifikáty
+Problémy s OS Ubuntu
+Problémy s OS Ubuntu
+Práce nebo potíže související s rookem
+Nedostupná síť přes VPN
+Nastavení Daphne
+Potíže související s notebookem
+Vytvoření e-mailového aliasu
+Problémy s OS Ubuntu
+Problémy s OS Ubuntu
+Práce nebo potíže související s rookem
+Práce souvisejíci s mailing listem
+Nedostupná síť přes VPN
+Potíže se servery
+Připojení telefonu do vnitřní sítě (cerifikáty)
+Vytvoření nebo prodloužení certifikátu na VPN
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Příslušenství k notebooku (baterie, tašky)
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Příslušenství k notebooku (baterie, tašky)
+Vytvoření nebo prodloužení certifikátu na VPN
+Pořízení nového HW
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Příslušenství k notebooku (baterie, tašky)
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vytvoření nebo prodloužení certifikátu na VPN
+Problémy s e-maily
+Problémy s tiskárnami
+Vytvoření e-mailového aliasu
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vybaveni pracoviště
+Vybaveni pracoviště
+Vybaveni pracoviště
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Problémy s e-maily
+Problémy s e-maily
+Problémy s tiskárnami
+Problémy s tiskárnami
+Problémy s tiskárnami
+Problémy s e-maily
+Příprava validačního notebooku
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Vylepšení dokumentace pro webdavs
+Pořízení nového HW
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Zobrazení monitoringu na PC
+Odstranění/přidání/úprava uživatelů v systémech (crewatch, mapa)
+Makra na PC zpejsova
+Problémy s tiskárnami
+Pořízení nového HW
+'''
+import collections
+
+rx = sorted((list(collections.Counter(ex.splitlines()).items())), key=lambda x: x[1], reverse=True)
+for r in rx:
+    print '%s' %(r[0])+ '; ' '%d' %(r[1])
+
+
+
+
+
+
